7e18105d09827ade558c4dca0871f901c1c5f2ff
rajoy99 <rafat.joy99@gmail.com>
2022-02-14 13:01:10 +0600

    first commit

---
+++ models/note.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb+srv://joy:notetaker@cluster0.hprcc.mongodb.net/note-app?retryWrites=true&w=majority'
+
+console.log(' DB Connection : connecting to', url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
