8c3a2b99dda3f64f950f7ff59556bb51eba77350
rajoy99 <rafat.joy99@gmail.com>
2022-02-14 13:31:59 +0600

    react files added

---
+++ React Files/src/App.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react'
+import Note from './components/Note'
+import noteService from './services/notes'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { Button } from '@mui/material';
+import './App.css'
+
+const App = () => {
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
+
+
+  
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+      setNotes(initialNotes)
+    })
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+        .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+    .update(id, changedNote)
+      .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
+    .catch(error => {
+      alert(
+        `the note '${note.content}' was already deleted from server`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })    
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important == true);
+
+  return (
+    <div className="maindiv">
+      <h1 align='center'> Note :: Drop 💧 </h1>
+
+      <div>
+        <Button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </Button>
+      </div>
+      <List>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </List>
+      <form onSubmit={addNote} align="center">
+        <input value={newNote} onChange={handleNoteChange} />
+        <br></br>
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ marginTop: 3, backgroundColor: "#04b1d4" }}
+        >
+          SUBMIT
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default App
